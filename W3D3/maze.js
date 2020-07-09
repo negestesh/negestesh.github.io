ad43d366be9023af050b996cf3505eef19a39f8e
negestesh <nteshome@miu.edu>
2020-07-08 21:16:51 -0500

    add w3d3 lab

---
+++ W3D3/maze.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+  const backgroundColor = '#EEEEEE';
+  const redColor = '#FF8888';
+  let wallsHit = false;
+  let cheating = false;
+  function setText() {
+    $('#status').text('Click the "S" to begin.');
+  }
+  function turnRed() {
+    $('.boundary').addClass('youlose');
+    wallsHit = true;
+  }
+  function turnNormal() {
+    cheating = true;
+    wallsHit = false;
+    $('#status').css('visibility', 'visible');
+    $('#status').text('-----------------');
+    $('.boundary').removeClass('youlose');
+  }
+  function won() {
+    if (wallsHit) {
+      $('#status').show();
+      $('#status').text('Sorry, you lost. :[ Click "S" to begin again.');
+    } else {
+      $('#status').show();
+      $('#status').text('You win! :] Click "S" to begin again.');
+      $('.boundary').removeClass('youlose');
+    }
+  }
+  function cheater() {
+    if (cheating) {
+      turnRed();
+      won();
+    }
+  }
+  $('#bounds,h2').mouseover(cheater);
+  $('#end').click(won);
+  $('#start').click(turnNormal);
+  $('.boundary').mouseover(turnRed);
+};
